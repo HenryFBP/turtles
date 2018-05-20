bd8b3afdd46a166e2da3bb6e186ab72f329b5b93
Henry Post <HenryFBP@gmail.com>
2018-05-20 13:21:34 -0500

    add ascii art coloring function

--- src/TestTurtlePixel.java
+++ src/TestTurtlePixel.java
@@ -1,22 +1,44 @@
 import java.awt.Color;
+import java.util.HashMap;
 
 import TurtleGraphics.TGPoint;
 
 public class TestTurtlePixel
 {
+    @SuppressWarnings("serial")
     public static void main(String[] args)
     {
         Color[] rainbow = { Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA, };
         Color[] blackwhite = { Color.BLACK, Color.WHITE, Color.BLACK, Color.WHITE, Color.BLACK, Color.WHITE };
 
-        TurtlePixel t = new TurtlePixel();
+        HashMap<Character, Color> colormap = new HashMap<Character, Color>()
+        {
+            {
+                put('.', Color.PINK);
+                put('#', Color.YELLOW);
+                put('A', Color.GREEN);
+            }
+        };
+        
+        //@formatter:off
+        String happyfacestr = ""
+                + ".....\n"
+                + ".#.#.\n"
+                + "..A..\n"
+                + "#...#\n"
+                + ".###.\n";
+        //@formatter:on
 
+        Color[][] happyface = TurtlePixel.ASCIItoColor(happyfacestr, colormap);
+
+        TurtlePixel t = new TurtlePixel();
         t.throttle(100);
         int i = 0;
 
         t.drawPixel(Color.PINK, new TGPoint(0, (t.scale * i--)));
         t.drawPixels(rainbow, new TGPoint(0, (t.scale * i--)));
         t.drawPixels(blackwhite, new TGPoint(0, (t.scale * i--)));
+        t.drawPixels(happyface, new TGPoint(0, (t.scale * i--)));
 
     }
 
--- src/TurtlePixel.java
+++ src/TurtlePixel.java
@@ -1,10 +1,36 @@
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import TurtleGraphics.TGPoint;
 
 @SuppressWarnings("serial")
 public class TurtlePixel extends BetterTurtle
 {
+    public static Color[][] ASCIItoColor(String ascii, HashMap<Character, Color> color_map)
+    {
+        ArrayList<ArrayList<Color>> image = new ArrayList<>();
+        ArrayList<Color> row = new ArrayList<>();
+
+        for(Character c : ascii.toCharArray())
+        {
+            if(c == '\n')
+            {
+                image.add(row); // add row
+                row = new ArrayList<>(); // new row
+            }
+            else
+            {
+                row.add(color_map.get(c)); // add char
+            }
+        }
+
+        Color[][] imagec = image.stream().map(u -> u.toArray(new Color[0])).toArray(Color[][]::new); // tl;dr is { ArrayList<ArrayList<Color>> -> Color[][] }
+
+        return imagec;
+
+    }
+
     public int scale = 50;
 
     public TurtlePixel()
@@ -44,6 +70,17 @@ public class TurtlePixel extends BetterTurtle
         }
 
         penup();
+    }
+
+    public void drawPixels(Color[][] image, TGPoint l)
+    {
+        int r = (int) l.y;
+
+        for(Color[] row : image)
+        {
+            drawPixels(row, new TGPoint(l.x, r));
+            r -= scale;
+        }
     }
 
 }
