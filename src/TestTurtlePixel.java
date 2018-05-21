d08de1e49f399e89983857c59ea975504dd6a21c
Henry Post <HenryFBP@gmail.com>
2018-05-20 21:08:25 -0500

    add ants

--- src/TestTurtlePixel.java
+++ src/TestTurtlePixel.java
@@ -15,12 +15,13 @@ public class TestTurtlePixel
         List<List<Color>> hello =           LibPixel.ASCIItoColor(      "resources/hello.txt",        "resources/hello.map");
         List<List<Color>> happyfacepng =    LibPixel.imageFileToColor(  "resources/happyface.png");
         List<List<Color>> awwyeah =         LibPixel.imageFileToColor(  "resources/awwyeah.png");
+        List<List<Color>> ants =            LibPixel.imageFileToColor(  "resources/ants.png");
         //@formatter:on
 
-        TurtlePixel t = new TurtlePixel(1900, 1000, 3);
-        t.throttle(1);
+        TurtlePixel t = new TurtlePixel(1900, 1000, 2);
+        t.throttle(0);
 
-        int x = -600;
+        int x = -900;
         int y = 100;
 
         t.drawPixel(Color.PINK, new TGPoint(x, (t.scale * (y -= 1))));
@@ -30,6 +31,7 @@ public class TestTurtlePixel
         t.drawPixels(hello, new TGPoint(x, (t.scale * (y -= (happyface.get(0).size()))))); // go down by 1 column...\
         t.drawPixels(happyfacepng, new TGPoint(x, (t.scale * (y -= hello.get(0).size()))));
         t.drawPixels(awwyeah, new TGPoint(x, (t.scale * (y -= happyfacepng.get(0).size()))));
+        t.drawPixels(ants, new TGPoint(x, (t.scale * (y -= ants.get(0).size()))));
     }
 
 }
